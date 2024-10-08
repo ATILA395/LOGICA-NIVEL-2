d7da23feec67d73a8d498ec0db54df5c07683f4f
Átila <atilasantosdemello@gmail.com>
2024-10-08 19:07:26 -0300

    Create Exer-4.py

---
+++ Exer-4.py
@@ -0,0 +1,8 @@
+# Passo 1: Ler um valor numérico do teclado
+valor = float(input("Digite um valor numérico: "))
+
+# Passo 2: Calcular o antecessor
+antecessor = valor - 1
+
+# Passo 3: Escrever o resultado na tela
+print("O antecessor de", valor, "é", antecessor)
