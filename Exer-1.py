be5ed501da84d668aa0f7359c5d798da1e6d61d8
Átila <atilasantosdemello@gmail.com>
2024-10-08 18:58:58 -0300

    Create Exer-1.py

---
+++ Exer-1.py
@@ -0,0 +1,12 @@
+# Inicialização das variáveis
+A = 10
+B = 20
+
+# Troca dos valores
+temp = A  # Armazena o valor de A em uma variável temporária
+A = B     # Atribui o valor de B a A
+B = temp  # Atribui o valor armazenado em temp a B
+
+# Exibição dos resultados
+print("A =", A)
+print("B =", B)
