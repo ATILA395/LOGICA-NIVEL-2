dee5e83b8f64cfd62423d4439921aeacc23f2afd
Átila <atilasantosdemello@gmail.com>
2024-10-08 19:10:53 -0300

    Create Exer-6.py

---
+++ Exer-6.py
@@ -0,0 +1,9 @@
+# Passo 1: Ler as dimensões do retângulo
+base = float(input("Digite a base do retângulo: "))
+altura = float(input("Digite a altura do retângulo: "))
+
+# Passo 2: Calcular a área
+area = base * altura
+
+# Passo 3: Escrever o resultado
+print("A área do retângulo é:", area)
