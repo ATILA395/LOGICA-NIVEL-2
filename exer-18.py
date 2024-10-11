ff9ffd4897d4bfec532a1d00cd42f8edb3e279ef
Átila <atilasantosdemello@gmail.com>
2024-10-11 20:08:24 -0300

    Create exer-18.py

---
+++ exer-18.py
@@ -0,0 +1,9 @@
+# Lê dois valores do usuário
+valor1 = float(input("Digite o primeiro valor: "))
+valor2 = float(input("Digite o segundo valor: "))
+
+# Verifica e imprime o maior valor
+if valor1 > valor2:
+    print(f"O maior valor é: {valor1}")
+else:
+    print(f"O maior valor é: {valor2}")
