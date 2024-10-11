3fee8f67c776d800ce4e9ce2a8720aa2664e6a3a
Átila <atilasantosdemello@gmail.com>
2024-10-11 20:23:16 -0300

    Create exer-19.py

---
+++ exer-19.py
@@ -0,0 +1,9 @@
+# Lê dois valores
+valor1 = float(input("Digite o primeiro valor: "))
+valor2 = float(input("Digite o segundo valor: "))
+
+# Verifica e imprime em ordem crescente
+if valor1 < valor2:
+    print(f"Ordem crescente: {valor1}, {valor2}")
+else:
+    print(f"Ordem crescente: {valor2}, {valor1}")
